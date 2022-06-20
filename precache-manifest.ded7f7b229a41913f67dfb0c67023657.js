17b66300fa5e2ed64a61978547227aff753629ff
Samantha Puterman <Samanpg@bu.edu>
2022-06-20 11:26:08 -0400

    Updates

---
+++ precache-manifest.ded7f7b229a41913f67dfb0c67023657.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "43f8b9594f7f1a5f52d1",
+    "url": "/static/css/main.5510a4eb.chunk.css"
+  },
+  {
+    "revision": "43f8b9594f7f1a5f52d1",
+    "url": "/static/js/main.43f8b959.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "54ebca62cc728b8af6829fe78041df86",
+    "url": "/index.html"
+  }
+];
